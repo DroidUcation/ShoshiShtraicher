a81673d6d53670acc0246edba3914f3ee88a3df7
Shoshana Levin <Shoshana.Levin@software.dell.com>
2016-04-20 09:45:43 +0300

    HW2: 5 facts

---
+++ CokeFacts/app/src/main/java/com/course/cokefacts/FactsPageAdapter.java
@@ -0,0 +1,26 @@
+package com.course.cokefacts;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Facts Page Adapter
+ */
+public class FactsPageAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+    public FactsPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
